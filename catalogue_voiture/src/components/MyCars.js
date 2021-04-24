99f1e7fd0242bb7ca595d21a947632fa02f433d7
Benoit <benoitredondo@gmail.com>
2021-04-24 10:04:05 +0200

    create APP.JS + components Cars and MyCars

---
+++ catalogue_voiture/src/components/MyCars.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MyCars = () => {
+
+    return(
+        <>
+        <div>CATALOGUE DE VOITURES</div>
+        <p>Choisissez votre voiture</p>
+        </>
+    )
+}
+
+export default MyCars;
